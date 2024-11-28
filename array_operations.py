e7dd29ad8546448136bde7e6d4b272cf4862b3a9
ValerieMbaka <valeriembaka0@gmail.com>
2024-11-28 11:57:38 +0300

    updated array operations

--- array_operations.py
+++ array_operations.py
@@ -23,3 +23,64 @@ print(myNewArray)
 
 # Declare a single characters array
 # myChar = array('u', ['A', 'B', 'C'])
+
+# Accessing a single element in an array
+print(myArray[3])
+
+# Traverse elements in an array
+# for num in myArray:
+#         print(num)
+        
+# Function to traverse and print elements of an array
+def traverse_array(arr):
+        for num in arr:
+                print(num)
+                
+traverse_array(myArray)
+traverse_array(myNewArray)
+
+# Function to access accessing item in an array
+def accessByIndex(arr, index):
+        assert index >= 0 and index < len(arr), "The index must be positive and within the range of the array length"
+        print(arr[index])
+        
+accessByIndex(myArray, 3)
+
+# Function to search for an element in an array
+def search_array(arr, searchValue):
+        for num in arr:
+                if num == searchValue:
+                        return num
+        # If value is not found
+        return "The element does not exist in the array"
+
+search_array(myArray, 9)
+
+# Function to search for an element in an array and return both the index and element
+# def search_array(arr, searchValue):
+#         for index, num in enumerate(arr):
+#                 if num == searchValue:
+#                         return index
+#         # If value is not found
+#         return "The element does not exist in the array"
+#
+# search_array(myArray, 9)
+
+print(myArray)
+# Function to update an element in an array
+def update_array(arr, value, newValue):
+        index = search_array(arr, value)
+        if type(index) is int:
+                arr[index] = newValue
+        else:
+                print("Value not found")
+                
+update_array(myArray, 9, 15)
+print(myArray)
+
+# Function to delete an element in an array
+print(myNewArray)
+myNewArray.remove(5)
+myNewArray.pop(2)
+myNewArray.pop()
+print(myNewArray)
