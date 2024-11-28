ffa4ac970c887a1a232451a7d1334146a2873d6b
ValerieMbaka <valeriembaka0@gmail.com>
2024-11-28 14:32:11 +0300

    list operations

---
+++ dictionaries/dictionaries.py
@@ -0,0 +1,10 @@
+bigFive = {
+        'Lion' : 'Simba',
+        'Elephant' : 'Ndovu',
+        'Rhino' : 'Kifaru',
+        'Buffalo' : True,
+        'Leopard' : 'Chui'
+}
+print(bigFive)
+bigFive['Elephant'] = 'Tembo'
+print(bigFive['Elephant'])
