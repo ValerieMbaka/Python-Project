8915ec77df5384912c901211fda2934fb50c990d
ValerieMbaka <valeriembaka0@gmail.com>
2024-12-05 21:06:00 +0300

    linked list operations

--- linked_lists/linked_list_operations.py
+++ linked_lists/linked_list_operations.py
@@ -29,10 +29,57 @@ class SinglyLinkedList:
                         
                         # Point the head to the new node
                         self.head = newNode
+        
+        # Inserting a node at the end of the linked list
+        def insertAtEnd(self, data):
+                # Create a new node
+                newNode = Node(data)
+                # Check if the head is empty
+                if self.head is None:
+                        # Point the head to this new node
+                        self.head = newNode
+                else:
+                        # Traverse the linked list to find the last node
+                        last = self.head
+                        while last.next is not None:
+                                last = last.next
+                        # Point the last node to the new node
+                        last.next = newNode
+        
+        # 2. Deletion
+        # Function to delete a node in between the linked list
+        def deleteInBetween(self, value):
+                # Check if the linked list is empty
+                if self.head is None:
+                        print("The linked list is empty")
+                        return
                         
+                # Delete the head node
+                if self.head.data == value:
+                        current = self.head
+                        self.head = current.next
+                        return
+                
+                # Declare variables to hold the previous and current nodes
+                previous = None
+                current = self.head
+                
+                # Traverse the linked list to find the node to delete
+                while current is not None:
+                        if current.data == value:
+                                previous.next = current.next
+                                current.next = None
+                                del current
+                                return
+                        previous = current
+                        current = current.next
+                # Print a message if the value is not found
+                print("The value ", value, " was not found in the linked list")
+        
+        # The printList function traverses the linked list as it prints the data of each node
         def printList(self):
                 currentNode = self.head
-                while currentNode:
+                while currentNode is not None:
                         print(currentNode.data, end = " -> ")
                         currentNode = currentNode.next
                 print("None")
@@ -45,7 +92,14 @@ mySinglyLinkedList.insertAtBeginning('D')
 mySinglyLinkedList.insertAtBeginning('C')
 mySinglyLinkedList.insertAtBeginning('B')
 mySinglyLinkedList.insertAtBeginning('A')
+mySinglyLinkedList.insertAtEnd('E')
+mySinglyLinkedList.insertAtEnd('F')
 
 # Print the content of the linked list
 mySinglyLinkedList.printList()
         
+# Delete a node in between the linked list
+mySinglyLinkedList.deleteInBetween('C')
+
+# Print the new linked list
+mySinglyLinkedList.printList()
